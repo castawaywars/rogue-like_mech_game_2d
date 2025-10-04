8bdbe39e8dff04f44c14a1f55ad30e344315683e
castawaywars <freak.swiss@gmx.ch>
2025-10-04 11:12:46 +0200

    enable mechs to shoot

--- docs/scripts/player_triggered_functions.js
+++ docs/scripts/player_triggered_functions.js
@@ -71,7 +71,66 @@ function move_mech(mech_cell, mech, target_cell, direction) {
 }
 
 /**
- * TODO: shoot the weapons of a mech
+ * shoot the weapons of a mech
  * @param {String} mech 
  */
-function shoot(mech) { }
+function shoot(mech) {
+	let mech_tile = document.getElementById(mech);
+	if (mech_tile.classList.contains("up")) {
+		shot_flies(mech_tile.getAttribute("row"), mech_tile.getAttribute("column"), "up");
+	} else if (mech_tile.classList.contains("left")) {
+		shot_flies(mech_tile.getAttribute("row"), mech_tile.getAttribute("column"), "left");
+	} else if (mech_tile.classList.contains("down")) {
+		shot_flies(mech_tile.getAttribute("row"), mech_tile.getAttribute("column"), "down");
+	} else if (mech_tile.classList.contains("right")) {
+		shot_flies(mech_tile.getAttribute("row"), mech_tile.getAttribute("column"), "right");
+	}
+}
+
+/**
+ * recursive function to let a shot fly
+ * @param {number} from_row 
+ * @param {number} from_column 
+ * @param {string} direction 
+ */
+function shot_flies(from_row, from_column, direction) {
+	let target_row = from_row;
+	let target_column = from_column;
+	switch (direction) {
+		case "up":
+			target_row = from_row - 1;
+			break;
+		case "left":
+			target_column = from_column - 1;
+			break;
+		case "down":
+			target_row = (+from_row) + 1;
+			break;
+		case "right":
+			target_column = (+from_column) + 1;
+			break;
+	}
+	shot_hit_scan(target_row, target_column, direction);
+}
+
+/**
+ * checks if a shot hit something, if not keep flying, if yes remove enemy if needed
+ * @returns nothing
+ */
+function shot_hit_scan(target_row, target_column, direction) {
+	if (in_bounds(target_row, target_column)) {
+		let the_table = table_target();
+		let target = the_table.children[target_row].children[target_column];
+		if (target.classList.contains("0")) {
+			//nothing hit yet, continue flying
+			shot_flies(target_row, target_column, direction);
+		} else if (target.classList.contains("m") || target.classList.contains("n") || target.classList.contains("s") || target.classList.contains("w")) {
+			return; //hit an ally, spawner, or wall, stop flying
+		} else if (target.classList.contains("e")) {
+			tile_set_classes_to_zero(target);
+			return; //hit an enemy, stop flying
+		}
+	} else {
+		return; //hit the edge of the map, stop flying
+	}
+}
